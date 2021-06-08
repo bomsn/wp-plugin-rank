7bbbf0b22c03da83bd6c88bfeff780591158e0cf
Ali Khallad <bomsn@live.fr>
2021-06-08 14:16:05 +0100

    Fix: undefined property error

--- includes/templates/plugin-details.php
+++ includes/templates/plugin-details.php
@@ -4,22 +4,22 @@
 
         <tr>
             <th>Name</th>
-            <td><a href="https://wordpress.org/plugins/<?php echo $args->slug; ?>" target="_blank"><?php echo $args->name; ?></a></td>
+            <td><a href="https://wordpress.org/plugins/<?php echo $args->slug ?? WPPR_Slug; ?>" target="_blank"><?php echo $args->name ?? WPPR_Name; ?></a></td>
         </tr>
 
         <tr>
             <th>Slug</th>
-            <td><?php echo $args->slug; ?></td>
+            <td><?php echo $args->slug ?? WPPR_Slug; ?></td>
         </tr>
 
         <tr>
             <th>Rating</th>
-            <td><?php echo $args->rating; ?>%</td>
+            <td><?php echo $args->rating ?? '0'; ?>%</td>
         </tr>
 
         <tr>
             <th>Downloads</th>
-            <td><?php echo $args->downloaded; ?></td>
+            <td><?php echo $args->downloaded ?? '0'; ?></td>
         </tr>
 
     </tbody>
